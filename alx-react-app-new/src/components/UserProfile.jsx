e9ec0f95ab0769220cbe5f80cbd66d6963022077
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-20 23:39:22 +0300

    first commit1212134668987

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
 function UserProfile(props) {
   return (
-    <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
-      <h2 style={{ color: 'blue', marginBottom: '8px' }}>{props.name}</h2>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'blue' }}>{props.name}</h2>
       <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
       <p>Bio: {props.bio}</p>
     </div>
-  )
+  );
 }
 
 export default UserProfile;
