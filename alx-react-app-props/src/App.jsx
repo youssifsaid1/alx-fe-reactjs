00675b845ae00d41b3719add3e8a46db03228f47
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-20 23:59:05 +0300

    first commit4346768788

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,9 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import UserContext from './UserContext';
-import UserProfile from './components/UserProfile';
-
 // ✅ استدعاء الكومبوننتس
 import Header from './components/Header'
 import MainContent from './components/MainContent'
@@ -14,7 +11,7 @@ import UserProfile from './components/UserProfile'
 import WelcomeMessage from './components/WelcomeMessage'
 import Counter from './components/Counter'
 
-// ✅ استدعاء الـ Context
+// ✅ استدعاء الـ Context والكومبوننت الخاص بيه
 import ProfilePage from './ProfilePage'
 import UserContext from './UserContext'
 
