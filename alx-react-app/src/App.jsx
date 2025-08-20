bdf1e16f03cc374e76371eb67a73a1223a686ad1
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-20 23:04:23 +0300

    reacat

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,7 +8,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="App">
+      {/* ✅ الكومبوننت الجديد */}
+      <WelcomeMessage />
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,6 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,7 +33,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
