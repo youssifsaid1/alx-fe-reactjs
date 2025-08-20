67f99a3bc0d6bdc8e10c7397720a7aa5fc6ad543
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-21 00:04:22 +0300

    first commit43467687885465611121215132

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,11 +3,9 @@ import UserContext from './UserContext';
 import ProfilePage from './ProfilePage';
 
 function App() {
-  // البيانات اللي هنبعتها
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    // نوفر البيانات للـ Context
     <UserContext.Provider value={userData}>
       <ProfilePage />
     </UserContext.Provider>
