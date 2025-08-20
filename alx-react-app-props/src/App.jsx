3a5a6f52b98b5d63b29f7e91cb7e2a37c3aff507
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-20 23:58:34 +0300

    first commit4346768788

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import UserContext from './UserContext';
+import UserProfile from './components/UserProfile';
+
 // ✅ استدعاء الكومبوننتس
 import Header from './components/Header'
 import MainContent from './components/MainContent'
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,14 @@
-function UserProfile(props) {
+import { useContext } from "react";
+import UserContext from "../UserContext";
+
+function UserProfile() {
+  // ✅ استدعاء البيانات من الـ Context بدل props
+  const userData = useContext(UserContext);
+
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <h2 style={{ color: 'blue' }}>{props.name}</h2>
-      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p>Bio: {props.bio}</p>
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }
