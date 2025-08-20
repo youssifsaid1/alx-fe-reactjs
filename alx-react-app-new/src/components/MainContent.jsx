09129cdefd51ad3d7946c27c27a442d5d07283ba
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-20 23:32:05 +0300

    first commit1212134

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,9 +1,9 @@
 function MainContent() {
   return (
-    <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+    <main style={{ backgroundColor: '#e6f7ff', padding: '20px', textAlign: 'center' }}>
+      <p style={{ fontSize: '18px' }}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
-  );
+  )
 }
 
 export default MainContent;
