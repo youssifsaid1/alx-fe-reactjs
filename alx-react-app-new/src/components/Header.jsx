ff416a653cc32f2620aa05ecd10c9f6000a4baad
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-20 23:36:26 +0300

    first commit121213466

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
-    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}>
       <h1>My Favorite Cities</h1>
     </header>
-  )
+  );
 }
 
 export default Header;
