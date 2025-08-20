2dde6dd940b2088b075af777ae7d2f69fe58fd40
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-20 23:38:42 +0300

    first commit1212134667

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
-    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px' }}>
+    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px' }}>
       <p>© 2023 City Lovers</p>
     </footer>
-  )
+  );
 }
 
 export default Footer;
