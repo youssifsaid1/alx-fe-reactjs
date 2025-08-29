b374e194aa8601c0779d230dde28f3b97e66d8ed
Eng-Youssif Said <youssifsaid>
2025-08-29 16:43:25 +0300

    111

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // ✅ أضف هذا السطر
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+        react(),
+        tailwindcss(), // ✅ أضف هذا السطر
+  ],
 })
